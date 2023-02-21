0863516413eb265040cc16361491d376a8ea14dc
NurGuru <nurchibald@mail.ru>
2023-02-21 14:26:33 +0300

    first commit

---
+++ alboms/main_js.js
@@ -0,0 +1,25 @@
+// комментарий не выполняется
+/* Кооментарии для себя или других разработчиков 
+
+*/
+const btnGet = document.getElementById("btnGetPosts");
+
+//для хранения полученных постов
+const state = {
+  posts: [],
+};
+
+btnGet.addEventListener("click", () => {
+  const input_count = document.getElementById("input_count");
+  GetPostsRequest(input_count.value);
+});
+
+function GetPostsRequest(count) {
+  url = "https://jsonplaceholder.typicode.com";
+  resources = "/albums";
+  target_url = url + resources + "?_limit=" + count;
+
+  fetch(target_url)
+    .then((response) => response.json())
+    .then((res_json) => console.log(res_json));
+}
